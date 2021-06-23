8537a09d9b521c9f4486b700e8c55d97915433bb
lilstex <53871487+lilstex@users.noreply.github.com>
2021-06-23 22:38:11 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 /**
- * print_alphabet x10 - prints each alphabet 10times
+ * print_alphabet_x10 - prints alphabet 10 times
  */
 void print_alphabet_x10(void)
 {
-  int i;
-  char a;
-  for(i = 0; i < 10; i++)
-    {
-      for(a = 'a'; a <= 'z'; a++)
+	int i;
+	char a;
+
+	for (i = 0; i < 10; i++)
 	{
-	  _putchar(c);
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
 	}
-      _putchar('\n');
-    }
 }
