e7d6da403b766c1e77af3aa589b5ab923b22ae31
lilstex <53871487+lilstex@users.noreply.github.com>
2021-06-24 09:59:11 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char character;
+for (character = 'a' ; character <= 'z' ; character++)
+{
+if (character != 'q' && character != 'e')
+{
+putchar(character);
+}
+}
+putchar('\n');
+return (0);
+}
