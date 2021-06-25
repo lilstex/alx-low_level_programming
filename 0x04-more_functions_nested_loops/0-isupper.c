3681a70daaf15692aaf62234d0c293b649acec4c
lilstex <53871487+lilstex@users.noreply.github.com>
2021-06-25 17:56:27 +0100

    Is upper Case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,19 @@
 #include "holberton.h"
+
+/**
+* _isupper - returns 1 if c is uppercase
+* @c: integer to be tested
+* Return: 1 or 0
+*/
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
