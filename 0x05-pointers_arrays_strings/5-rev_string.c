e31ae72e13e08420ea0944e7ab1f72d03cf1b5b2
lilstex <53871487+lilstex@users.noreply.github.com>
2021-06-28 21:36:15 +0100

    Update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,14 @@
 void rev_string(char *s)
 {
 
-int len, i, half;
-char temp;
-
-for (len = 0; s[len] != '\0'; len++)
-;
-
-i = 0;
-
-half = len / 2;
-
-while (half--)
-{
-	temp = s[len - i - 1];
-	s[len - i - 1] = s[i];
-	s[i] = temp;
+int i = 0;
+while (s[i])
 	i++;
+
+while (i--)
+{
+	_putchar(s[i]);
 }
+_putchar('\n');
 
 }
