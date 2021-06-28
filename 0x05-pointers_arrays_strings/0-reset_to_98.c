b0447ba45c3c173b56f206b59cb03bdd1501de1b
lilstex <53871487+lilstex@users.noreply.github.com>
2021-06-28 19:15:29 +0100

    Update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 /**
- * reset_to_98 - Resets the argument that points to the address to 98
- *
+ * reset_to_98 - Reset argument that points to the address to 98
+ * @n: pointer
  * Return: void
- */
+*/
+
 void reset_to_98(int *n)
 {
     *n = 98;
