9107791cc41cce77e3425f3d8f0344f47be3c551
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-01 20:41:46 +0100

    Compares strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,28 +1,20 @@
+  
 #include "holberton.h"
-/**
- * rot13 - convert to rot 13
- * @str: input string
- * Return: String conversion
- */
-char *rot13(char *str)
-{
-	int count = 0, x;
-	char *letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (str[count] != '\0')
+/**
+ * _strcmp - Compares s1 and s2
+ *
+ * @s1: String to be compared against s2
+ * @s2: String to be compared against s1
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while ((s1[i] == s2[i]) && (s1[i] != '\0'))
 	{
-		x = 0;
-		while (letters[x] != '\0')
-		{
-			if (str[count] == letters[x])
-			{
-				str[count] = rot13[x];
-				break;
-			}
-			x++;
-		}
-		count++;
+		i++;
 	}
-	return (str);
+	return (s1[i] - s2[i]);
 }
