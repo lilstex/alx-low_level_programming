534e04b9aa03b3b6fe6858664dccdccf2f497136
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-01 20:33:51 +0100

    String catenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,13 +13,13 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0') /*WHILE count character */
+	while (dest[i] != '\0') 
 	{
 		i++;
 	}
 
 	j = 0;
-	while (j < n && src[j] != '\0') /*WHILE concatenate dest with src*/
+	while (j < n && src[j] != '\0') 
 	{
 		dest[i] = src[j];
 		j++;
