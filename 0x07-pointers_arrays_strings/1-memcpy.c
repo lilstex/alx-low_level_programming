c00c0da68cd82f2c6c3168c2f943ddfa63bca62f
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-05 19:21:43 +0100

    copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,12 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
           int i;
-                      
-          for(i = 0, i < n, i++)
-                    *dest[i] = *s;
-  
-          return (*dest);    
+                     
+          i = 0;
+          while(i < n)
+          {
+                    dest[i] = src[i];
+          }
+          
+          return (dest);    
 }
