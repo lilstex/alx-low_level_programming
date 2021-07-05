caf0700c1ba7e87ec75ddbd8be97b29004dc0b77
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-05 17:57:00 +0100

    Memory set

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memset - Fills the first n bytes of the memory area
+ *          pointed to by @s with the constant byte @b.
+ * @s: A pointer to the memory area to be filled.
+ * @b: The character to fill the memory area with.
+ * @n: The number of bytes to be filled.
+ *
+ * Return: A pointer to the filled memory area @s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+          unsigned char *memory = s;
+          unsigned char value = b;
+  
+          for(i = 0, i < n, i++)
+                 memory[i] = b;
+  
+          return (memory);    
+}
