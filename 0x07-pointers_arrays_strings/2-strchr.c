72e16a3a81217a57fbca17a4b72d581ceade8c91
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-05 18:46:40 +0100

    Locate character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,11 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] != c; i++)
 	{
-		if (s[i] == c)
-			return (s + i);
+		if (s[i] == '\0')
+			return ('\0');
 	}
 
-	return ('\0');
+	return (s + i);
 }
