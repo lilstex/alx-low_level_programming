c12696d2130273fb7535466124c25d5ff678fbec
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-07 13:32:09 +0100

    Prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,15 +3,14 @@
 /**
  * _puts_recursion - Prints a string, followed by a new line
  * @s: The string to be printed
- * Return: 0
  */
 void _puts_recursion(char *s)
-{
-	if(*s)
-
-	putchar(*s);
+if (*s)
+	{
+		_putchar(*s);
 		_puts_recursion(s + 1);
-  else
-	
-  putchar('/n');
+	}
+
+	else
+		_putchar('\n');
 }
