326577f0b8c71eb7d2ef7053bcea5c0f5492897c
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-07 12:25:50 +0100

    print in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be copied
+ * Return: pointer to dest
+ */
+void _print_rev_recursion(char *s)
+{
+	char *mem = dest;
+
+	while (n--)
+		*dest++ = *src++;
+	return (mem);
+}
