49e0cbff60cbff92f78449ddd67edadfc501bd82
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-07 12:27:07 +0100

    print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,9 +3,7 @@
 /**
  * _puts_recursion - Prints a string, followed by a new line
  * @s: The string to be printed
- * @b: constant byte
- * @n: bytes of the memory area
- * Return: pointer to the memory area s
+ * Return: 0
  */
 void _puts_recursion(char *s)
 {
