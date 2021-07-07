c0aa8846a6c6721ca31360dc85d0c541a95b939d
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-07 12:20:20 +0100

    prints string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,19 @@
-ni
+#include "holberton.h"
+
+/**
+ * _puts_recursion - Prints a string, followed by a new line
+ * @s: The string to be printed
+ * @b: constant byte
+ * @n: bytes of the memory area
+ * Return: pointer to the memory area s
+ */
+void _puts_recursion(char *s)
+{
+	if(*s)
+
+	putchar(*s)
+		_puts_recursion(s + 1);
+  else
+	
+  putchar('/n');
+}
