1bcc154ff7f7b5eeab0a65153762f19e7a2f0c38
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-07 22:01:32 +0100

    length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,20 +1,19 @@
 #include "holberton.h"
+
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer the string
+ * Return: int
  */
 
-int _strlen_recursion(char *s)       
+int _strlen_recursion(char *s)
 {
-        int i = 0;
+int n = 0;
 
-        if (*s)
-        {
-                i++;
-                i += _strlen_recursion(s + 1);
-        }
+if (*s > '\0')
+{
+	n += _strlen_recursion(s + 1) + 1;
+}
 
-        return (i);
+return (n);
 }
