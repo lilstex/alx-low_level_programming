65b25746cd13bbc0d83e9c050642e32762d9eae3
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-12 20:15:46 +0100

    prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* main - prints the number of arguments passed into it
+* @argc: argument count
+* @argv: argument vector
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
