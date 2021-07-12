29aa45dc194d8bea2581090e12dacc4cda0dd691
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-12 20:54:31 +0100

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+* main - adds positive numbers
+* @argc: argument count
+* @argv: argument vector
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
