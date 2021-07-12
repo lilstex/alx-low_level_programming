dbd9154d7f5d9d84a29aa2cfeb3f73bae5f7ac95
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-12 20:36:46 +0100

    prints all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* main - prints all arguments passed it receives
+* @argc: argument count
+* @argv: argument vector
+* Return: 0
+*/
+int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
