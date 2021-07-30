e670ccf45ef62330d83d1debc0b3249bcee1ba5b
lilstex <53871487+lilstex@users.noreply.github.com>
2021-07-30 16:55:40 +0100

    adds a new node to the end of linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,21 @@
 #include <stdlib.h>
+#include <string.h>
 #include "lists.h"
+
+/**
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *
+ * Return: length of string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
 
 /**
  * add_node_end - adds a new node to the end of linked list
@@ -12,18 +28,27 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *tmp;
 
-	temp = malloc(sizeof(list_t));
-  temp->str = NULL;
-  if(head == NULL)
-    head = temp
-  else
-  {
-    new = head;
-    while(new->str != NULL)
-    {
-      new = new->str;
-    }
-    new->str = temp
-  }
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(new->str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
 	return (new);
 }
