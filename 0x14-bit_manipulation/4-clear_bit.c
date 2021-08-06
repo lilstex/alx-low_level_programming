18444db58a374ae60570839d597efb326665476b
lilstex <53871487+lilstex@users.noreply.github.com>
2021-08-06 19:27:42 +0100

    sets the value of a bit to 0 at a given index.

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 
 /**
- * clear_bit - Sets the value of a bit at a given index to 0.
- * @n: A pointer to the bit.
- * @index: The index to set the value at - indices start at 0.
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: number to set
+ * @index: index at which to set bit
  *
- * Return: If an error occurs - -1.
- *         Otherwise - 1.
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	unsigned long int set;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-
-	*n &= ~(1 << index);
-
+	set = ~(1 << index);
+	*n = *n & set;
 	return (1);
 }
