d951ba512f90cb69467efca32b41d86fdb35e075
lilstex <53871487+lilstex@users.noreply.github.com>
2021-08-06 19:24:39 +0100

    checks the endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,18 +1,16 @@
 #include "holberton.h"
 
 /**
- * get_endianness - Checks the endianness.
+ * get_endianness - checks the endianness
  *
- * Return: If big-endian - 0.
- *         If little-endian - 1.
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-	int num = 1;
-	char *endian = (char *)&num;
+	int x;
+	char *y;
 
-	if (*endian == 1)
-		return (1);
-
-	return (0);
+	x = 1;
+	y = (char *)&x;
+	return (*y);
 }
