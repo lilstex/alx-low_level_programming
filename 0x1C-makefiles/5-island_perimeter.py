396e43f7294d2b6fcf927c330dc86e0a2cd6da6c
Emmanuel <lilstex4good@gmail.com>
2021-11-26 22:12:32 +0100

    Completed and updated the makefile task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,4 +3,26 @@
 
 def island_perimeter(grid):
     ''' Documentation
+        grid is a list of list of integers:
+            0 represents a water zone
+            1 represents a land zone
+            One cell is a square with side length 1
+            Grid cells are connected horizontally/vertically (not diagonally).
+            Grid is rectangular, width and height don’t exceed 100
+            len(grid) = length of grid row
+            len(grid[x]) = length of grid column
     '''
+    size = 0
+    sides = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                size += 1
+                if x != 0 and grid[x - 1][y] == 1:
+                    sides += 1
+                if y != 0 and grid[x][y - 1] == 1:
+                    sides += 1
+
+    perimeter = 4 * size - 2 * sides
+     
+    return perimeter
